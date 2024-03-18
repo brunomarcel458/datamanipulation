b953e352757a9df2b8079136e0c4ff1ee4916915
brunoomarcel <brunomarcel458@gmail.com>
2024-03-18 20:08:24 -0300

    add dados_transparencia.csv archive

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import os.path
 
+
+from domain.archive import Archive
 
 if __name__ == '__main__':
-    pass
+    file_name = "transparencia.csv"
+    arquivo = Archive(file_name)
+    if os.path.exists(file_name):
+        pass
+    else:
+        arquivo.create_archive()
